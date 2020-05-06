74811688903cce3da93139fe7f0915d805bb25e2
Anurag Singh <anuragsingh20@live.com>
2020-05-06 21:49:30 +0530

    Form validation

--- react-test/src/components/NewPost/NewPost.js
+++ react-test/src/components/NewPost/NewPost.js
@@ -4,24 +4,48 @@ import Alert from 'react-bootstrap/Alert';
 import axios from 'axios';
 import Input from '../UI/Input/Input';
 
-class NewPost extends Component {
-    state = {
+const initialState = {
         title: '',
         body: '',
-        author: 'Anurag'
-    }
+        author: '',
+        titleError:'',
+        autorError:'',
+        bodyError: ''
+}
+class NewPost extends Component {
+    state = initialState;
 
-    postDataHandler = () => {
-        const dataToPost = {
-            title: this.state.title,
-            body: this.state.body,
-            author: this.state.author
+    validateForErrors = ()=>{
+
+        if(this.state.title === ''){
+            this.setState({titleError: 'Title is required'});
+            return false;
         }
 
-        axios.post("/posts", dataToPost)
-            .then(response => {
-                console.log(response);
-            })
+        return true;
+
+    }
+
+    postDataHandler = (event) => {
+event.preventDefault(); // to prevent form refresh
+        const isValid = this.validateForErrors();
+        if(isValid){
+            const dataToPost = {
+                title: this.state.title,
+                body: this.state.body,
+                author: this.state.author
+            }
+    
+            axios.post("/posts", dataToPost)
+                .then(response => {
+                    console.log(response);
+                    // clear the form on sucess validation and post
+                this.setState(initialState);
+                })
+
+                
+            }
+        
     }
 
     
@@ -30,10 +54,15 @@ class NewPost extends Component {
         return (
             <div>
                 <Alert variant="primary">Add a new post</Alert>
+                <form onSubmit={this.postDataHandler}>
                 <Input inputtype="input" type="text" placeolder="Enter input"  label="Enter title" value={this.state.title} onChange={(event)=>this.setState({title: event.target.value})}/>
+                <div style={{color: 'red'}}>
+                    {this.state.titleError}
+                </div>
                 <Input inputtype="input" type="text" placeolder="Enter input" label="Enter author" value={this.state.author} onChange={(event)=>this.setState({author: event.target.value})}/>
                 <Input inputtype="textarea" type="textarea" placeolder="Enter input" label="Enter body" value={this.state.body} onChange={(event)=>this.setState({body: event.target.value})}/>
-                <Button onClick={this.postDataHandler}>Post</Button>
+                <Button type="submit">Post</Button>
+                </form>
             </div>
         );
     }
